5113c7d0a2b2ecebea6b28859916374d8ed41b42
himanshu <himanshuchawla2014@gmail.com>
2018-02-06 23:56:28 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ParticlesModule } from 'angular-particle';
 
 import { AppComponent } from './app.component';
-
+import {NgsRevealModule} from 'ng-scrollreveal';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ParticlesModule,
+    NgsRevealModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
